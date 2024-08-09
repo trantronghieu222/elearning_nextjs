eb30c70969a0d893d55199a7f467ab9dd8b720d4
Tran Trong Hieu <tranhieu4345@gmail.com>
2024-08-09 23:13:21 +0700

    update css

--- elearning/src/app/components/UserDropdown/UserDropdown.js
+++ elearning/src/app/components/UserDropdown/UserDropdown.js
@@ -1,42 +1,54 @@
 import React from 'react';
 import { DownOutlined } from '@ant-design/icons';
 import { Dropdown, Space } from 'antd';
-import { getDataJsonStorage } from '@/app/util/function';
 import { handleLogout } from '@/app/server/action/users';
+import styles from '../../assets/css/Components/userDropdown.module.css';
 import Link from 'next/link';
+
 const UserDropdown = (props) => {
-  // const userLogin = getDataJsonStorage('userLogin');
   const { userLogin } = props;
 
-  // Khai báo items dựa vào maLoaiNguoiDung của userLogin
   let items = [
     {
-      label: <Link href='/thongtintaikhoan' className='text-decoration-none'>Thông tin</Link>,
+      label: <Link href='/thongtintaikhoan' className={styles.navLink}>Thông tin</Link>,
       key: '0',
     },
     {
-      label: <a onClick={handleLogout}>Đăng xuất</a>,
+      label: <a className={styles.navLink} onClick={handleLogout}>Đăng xuất</a>,
       key: '1',
     }
   ];
 
   if (userLogin && userLogin.maLoaiNguoiDung === 'GV') {
-    items.push({
-      type: 'divider'
-    }, {
-      label: <Link href="/admin/quanlykhoahoc" className='text-decoration-none'>Trang quản trị</Link>,
-      key: '3'
-    });
+    items.push(
+      { type: 'divider' },
+      {
+        label: <Link href="/admin/quanlykhoahoc" className={styles.navLink}>Trang quản trị</Link>,
+        key: '3'
+      }
+    );
   }
 
   return (
     <Dropdown
-      menu={{
-        items,
-      }}
+      overlay={
+        <div className={styles.dropdownMenu}>
+          {items.map(item => (
+            item.type === 'divider' ? <hr key={item.key} className={styles.divider} /> :
+            <div 
+              key={item.key} 
+              className={styles.dropdownItem}
+              onMouseEnter={(e) => e.currentTarget.classList.add(styles.dropdownItemHover)}
+              onMouseLeave={(e) => e.currentTarget.classList.remove(styles.dropdownItemHover)}
+            >
+              {item.label}
+            </div>
+          ))}
+        </div>
+      }
       trigger={['click']}
     >
-      <a onClick={(e) => e.preventDefault()} className='text-dark text-decoration-none'>
+      <a onClick={(e) => e.preventDefault()} className={styles.dropdownTrigger}>
         <Space>
           {userLogin.hoTen}
           <DownOutlined />
